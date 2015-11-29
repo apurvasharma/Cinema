855f35ca7ae3e247f788ba8e1dc054fb000fad3d
apurvasharma <apurva.may21@gmail.com>
2015-11-29 18:54:08 -0500

    modify navigation drawer options

---
+++ app/src/main/java/com/cinema/movie/info/utils/AppConstants.java
@@ -0,0 +1,9 @@
+package com.cinema.movie.info.utils;
+
+/**
+ * Created by Apurva on 11/29/2015.
+ */
+public class AppConstants {
+    public static final String URL = "";
+    public static final String API_KEY = "";
+}
