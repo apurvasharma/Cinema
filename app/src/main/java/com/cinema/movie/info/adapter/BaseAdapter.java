47cf13ba241c3ed4fb32d77bfc36bfc47e507308
apurvasharma <apurva.may21@gmail.com>
2015-12-30 16:14:46 +0530

    Abstract common styles

---
+++ app/src/main/java/com/cinema/movie/info/adapter/BaseAdapter.java
@@ -0,0 +1,7 @@
+package com.cinema.movie.info.adapter;
+
+/**
+ * Created by Apurva on 12/30/2015.
+ */
+public class BaseAdapter {
+}
---
+++ app/src/main/java/com/cinema/movie/info/fragments/BaseFragment.java
@@ -0,0 +1,7 @@
+package com.cinema.movie.info.fragments;
+
+/**
+ * Created by Apurva on 12/30/2015.
+ */
+public class BaseFragment {
+}
